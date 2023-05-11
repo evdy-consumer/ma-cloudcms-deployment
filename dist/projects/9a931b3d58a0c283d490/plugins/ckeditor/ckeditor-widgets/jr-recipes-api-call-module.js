4333024e0f3b29b2281a5d8680053f9f98b63aae
svc-eh-jenkins <svc-eh-jenkins@waterfrontmedia.net>
2023-05-11 09:21:26 -0400

    chore(release): Updated modules

--- dist/projects/9a931b3d58a0c283d490/plugins/ckeditor/ckeditor-widgets/jr-recipes-api-call-module.js
+++ dist/projects/9a931b3d58a0c283d490/plugins/ckeditor/ckeditor-widgets/jr-recipes-api-call-module.js
@@ -26624,7 +26624,9 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;function _typeof
                 schema = config.schema,
                 options = config.options,
                 data = config.data,
-                observableHolder = config.observableHolder;
+                observableHolder = config.observableHolder,
+                _config$postRenderCal = config.postRenderCallback,
+                postRenderCallback = _config$postRenderCal === void 0 ? function () {} : _config$postRenderCal;
 
             if (!title) {
               reject(new Error('Missing title'));
@@ -26644,6 +26646,7 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;function _typeof
                 options: options,
                 data: data,
                 postRender: function postRender(control) {
+                  postRenderCallback(control);
                   $done.on('click', function () {
                     control.refreshValidationState(true, function () {
                       if (!control.isValid(true)) {
@@ -33977,7 +33980,7 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;function _typeof
             };
 
             console.log('\n----dashlet config---\n', {
-              dashletConfig: JSON.stringify({
+              dashletConfig: _objectSpread(_objectSpread({}, dashletConfig), {}, {
                 postRender: postRender
               })
             });
